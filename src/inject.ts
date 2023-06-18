ba2f5a4dc451029eecf4fc40d90fc760eb5b872a
uncenter <me@uncenter.org>
2023-06-17 20:28:59 -0400

    refactor: more native looking injected content

--- src/inject.ts
+++ src/inject.ts
@@ -79,7 +79,7 @@ export function injectContent(pkg: Package, refresh = false) {
 		li = document.querySelector('.npm-stats') as HTMLLIElement;
 	}
 	li.innerHTML = `
-    <div data-view-component="true" class="starred BtnGroup flex-1">
+    <div data-view-component="true" class="d-flex">
     <a href="https://www.npmjs.com/package/${
 			pkg.name
 		}" target="_blank" class="btn btn-sm btn-with-count tooltipped tooltipped-s BtnGroup-item" aria-label="View package on npmjs.com" ata-view-component="true">
@@ -118,6 +118,7 @@ export function injectContent(pkg: Package, refresh = false) {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"></path></svg>
             </button>
         </div>
+        <div class="select-menu-content">
         <dl>
           <dt>Last day</dt>
           <dd>${pkg.stats.lastDay.toLocaleString()}</dd>
@@ -127,6 +128,7 @@ export function injectContent(pkg: Package, refresh = false) {
           <dd>${pkg.stats.lastMonth.toLocaleString()}</dd>
         </dl>
         <canvas id="npm-stats-chart"></canvas>
+        </div>
       </details-menu>
     </details>
     </div>
