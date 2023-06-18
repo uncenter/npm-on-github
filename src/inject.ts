34aa14ee7f65938bbe7f5f5b52c04f3de76b84aa
uncenter <me@uncenter.org>
2023-06-17 23:42:22 -0400

    refactor: make close button close modal, rewrite modal to be native github ui

--- src/inject.ts
+++ src/inject.ts
@@ -101,7 +101,7 @@ export function injectContent(pkg: Package, refresh = false) {
                 ${formatNumber(pkg.stats.lastDay)}
             </span>
     </a>
-    <details class="details-reset details-overlay BtnGroup-parent js-user-list-menu d-inline-block position-relative"${
+    <details id="npm-stats-details" class="details-reset details-overlay BtnGroup-parent js-user-list-menu d-inline-block position-relative"${
 			refresh ? ' open' : ''
 		}>
       <summary class="btn-sm btn BtnGroup-item px-2 float-none" aria-haspopup="menu" role="button" aria-label="View NPM downloads graph for ${
@@ -111,24 +111,33 @@ export function injectContent(pkg: Package, refresh = false) {
     <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"></path>
 </svg>
       </summary>
-      <details-menu class="select-menu-modal position-absolute">
-        <div class="select-menu-header">
-          <h5>Downloads</h5>
-          <button id="npm-stats-refresh" type="button" class="btn tooltipped tooltipped-w" aria-label="Refresh">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"></path></svg>
-            </button>
-        </div>
-        <div class="select-menu-content">
-        <dl>
-          <dt>Last day</dt>
-          <dd>${pkg.stats.lastDay.toLocaleString()}</dd>
-          <dt>Last week</dt>
-          <dd>${pkg.stats.lastWeek.toLocaleString()}</dd>
-          <dt>Last month</dt>
-          <dd>${pkg.stats.lastMonth.toLocaleString()}</dd>
-        </dl>
-        <canvas id="npm-stats-chart"></canvas>
-        </div>
+      <details-menu class="select-menu-modal position-absolute" style="animation-name: SelectMenu-modal-animation--sm;">
+            <header class="SelectMenu-header">
+                <h5 class="SelectMenu-title f5">Downloads</h5>
+                <button id="npm-stats-close" type="button" class="SelectMenu-closeButton" aria-label="Close menu">
+                    <svg viewBox="0 0 16 16" height="16" width="16" data-view-component="true" class="octicon octicon-x" aria-hidden="true"><path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path></svg>
+                </button>
+            </header>
+            <div class="select-menu-content">
+                <dl>
+                    <dt>Last day</dt>
+                    <dd>${pkg.stats.lastDay.toLocaleString()}</dd>
+                    <dt>Last week</dt>
+                    <dd>${pkg.stats.lastWeek.toLocaleString()}</dd>
+                    <dt>Last month</dt>
+                    <dd>${pkg.stats.lastMonth.toLocaleString()}</dd>
+                </dl>
+                <canvas id="npm-stats-chart"></canvas>
+            </div>
+            <footer class="SelectMenu-footer px-2">
+                <button id="npm-stats-refresh" type="button" data-view-component="true" class="user-lists-menu-action js-user-lists-create-trigger btn-invisible btn btn-block text-normal rounded-1 px-2"${
+									new Date().getTime() - pkg.lastChecked < 86400000
+										? ' disabled'
+										: ''
+								}>
+                    <svg viewBox="0 0 16 16" width="16" height="16" data-view-component="true" class="octicon octicon-sync"><path d="M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"></path></svg> Refresh stats 
+                </button>
+            </footer>
       </details-menu>
     </details>
     </div>
@@ -145,5 +154,12 @@ export function injectContent(pkg: Package, refresh = false) {
 			} else {
 				logger.warn('Failed to refresh stats.');
 			}
+		});
+	injectionPoint
+		?.querySelector('#npm-stats-close')
+		?.addEventListener('click', () => {
+			injectionPoint
+				?.querySelector('#npm-stats-details')
+				?.removeAttribute('open');
 		});
 }
