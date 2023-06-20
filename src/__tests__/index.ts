182ef14571d2ac93a8628a2a351f3511739a46ab
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-20 01:38:39 -0400

    test: add more tests

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,4 +1,5 @@
-import { getOwnerAndRepo } from '../utils';
+import { isFresh } from '../cache';
+import { formatNumber, getOwnerAndRepo } from '../utils';
 
 describe('getOwnerAndRepo', () => {
 	it('should return owner and repo', () => {
@@ -26,12 +27,56 @@ describe('getOwnerAndRepo', () => {
 			getOwnerAndRepo('git+https://github.com/uncenter/npm-on-github'),
 		).toEqual({ owner: 'uncenter', repo: 'npm-on-github' });
 	});
-	it('should rturn owner and repo if .git is in the middle', () => {
+	it('should return owner and repo if .git is in the middle', () => {
 		expect(
 			getOwnerAndRepo('git+https://github.com/example/.gitingore'),
 		).toEqual({ owner: 'example', repo: '.gitingore' });
 	});
 	it('should return null if no owner or repo', () => {
 		expect(getOwnerAndRepo('git+https://github.com')).toEqual(null);
+	});
+});
+
+describe('formatNumber', () => {
+	it('should format numbers', () => {
+		expect(formatNumber(1000000000)).toEqual('1b');
+		expect(formatNumber(1000000)).toEqual('1m');
+		expect(formatNumber(1000)).toEqual('1k');
+		expect(formatNumber(100)).toEqual('100');
+	});
+});
+
+describe('cache', () => {
+	describe('isFresh', () => {
+		it('should return true if cache is fresh', () => {
+			expect(
+				isFresh(
+					{
+						owner: 'uncenter',
+						repo: 'npm-on-github',
+						lastChecked: Date.now(),
+					},
+					{
+						displayPeriod: 'lastDay',
+						cacheDuration: 1,
+					},
+				),
+			).toEqual(true);
+		});
+		it('should return false if cache is not fresh', () => {
+			expect(
+				isFresh(
+					{
+						owner: 'uncenter',
+						repo: 'npm-on-github',
+						lastChecked: Date.now() - 2 * 24 * 60 * 60 * 1000,
+					},
+					{
+						displayPeriod: 'lastDay',
+						cacheDuration: 1,
+					},
+				),
+			).toEqual(false);
+		});
 	});
 });
