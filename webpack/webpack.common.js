919ebc1e1770768055a6e9fd1d3a720877f6ffd4
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-22 13:35:32 -0400

    style: prettier

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,40 +1,39 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = path.join(__dirname, "..", "src");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: {
-        content_script: path.join(srcDir, "index.ts"),
-    },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks(chunk) {
-                return chunk.name !== "background";
-            },
-        },
-        minimize: false,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
-    ],
+	entry: {
+		content_script: path.join(__dirname, '..', 'src', 'index.ts'),
+	},
+	output: {
+		path: path.join(__dirname, '../dist/js'),
+		filename: '[name].js',
+	},
+	optimization: {
+		splitChunks: {
+			name: 'vendor',
+			chunks(chunk) {
+				return chunk.name !== 'background';
+			},
+		},
+		minimize: false,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: '.', to: '../', context: 'public' }],
+			options: {},
+		}),
+	],
 };
