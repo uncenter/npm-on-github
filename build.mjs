91c231953e7babb29a81a0f4c5b92f85ca2b8be7
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-25 23:23:39 -0400

    feat: switch to esbuild

---
+++ build.mjs
@@ -0,0 +1,15 @@
+import { build } from 'esbuild';
+import { copyPlugin } from './copy.mjs';
+
+await build({
+	entryPoints: [{ out: 'js/content_script', in: 'src/index.ts' }],
+	bundle: true,
+	minify: true,
+	outdir: 'dist',
+	plugins: [
+		copyPlugin({
+			src: './public/',
+			dest: './dist/',
+		}),
+	],
+});
---
+++ copy.mjs
@@ -0,0 +1,67 @@
+// https://github.com/sprout2000/esbuild-copy-plugin
+
+// The MIT License (MIT)
+
+// Copyright (c) 2021-2022 Nick Janetakis <nick.janetakis@gmail.com>, sprout2000 and other contributors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
+const getDigest = (string) => {
+	const hash = crypto.createHash('md5');
+	const data = hash.update(string, 'utf-8');
+
+	return data.digest('hex');
+};
+
+const getFileDigest = (path) => {
+	if (fs.existsSync(path) && !fs.statSync(path).isDirectory()) {
+		return getDigest(fs.readFileSync(path).toString());
+	}
+	return null;
+};
+
+const filter = (src, dest) => {
+	if (fs.existsSync(dest) && !fs.statSync(dest).isDirectory()) {
+		return getFileDigest(src) !== getFileDigest(dest);
+	}
+	return true;
+};
+
+export const copyPlugin = (options = {}) => ({
+	name: 'esbuild-copy-plugin',
+	setup(build) {
+		const src = options.src;
+		const dest = options.dest;
+		if (!src || !dest) throw new Error('Source and/or destination option(s) are undefined.');
+
+		build.onEnd(() =>
+			fs.cpSync(src, dest, {
+				dereference: options.dereference || false,
+				errorOnExist: options.errorOnExist || false,
+				filter: options.filter || filter,
+				force: options.force || true,
+				preserveTimestamps: options.preserveTimestamps || false,
+				recursive: options.recursive || true,
+				verbatimSymlinks: options.verbatimSymlinks || false,
+			}),
+		);
+	},
+});
